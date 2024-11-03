f55fb157560182db54aef7b1a3a7a8b261b4dd61
ArthurRosin <arthur.rosin@aluno.ufabc.edu.br>
2024-11-02 23:31:02 -0300

    Aula de Linguagem C básica - Conceitos

---
+++ PE/firstProgram/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //Comentários em linhas
+
+    /*
+    Comentários em blocos
+    */
+
+    printf("Hello world!\n\n");
+
+    // "/n" Salto de linhas - usado dentro dos parênteses
+
+    printf("\nRetornar um valor: %d", printf("ola"));
+    //O print retorna a quantidade de caracteres na paralvra "ola" nesse caso
+
+    //Leitura de numeros inteiros
+    int valor, valorNovo;
+
+    valor = 50;
+
+    printf("\n\nDigite um valor inteiro: ");
+    scanf("%d", &valorNovo);
+    printf("\n\nPrimeiro valor: %d\nSegundo valor: %d", valor, valorNovo);
+
+        //system("pause") só funciona no windows, segue outra alternativa
+    printf("\n\nPressione qualquer tecla para finalizar: ");
+    getchar();
+
+    return 0;
+}
